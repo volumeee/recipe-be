2766862b05de31ffde43da788f7d346dab957fe7
Bagus Erwanto <57589007+volumeee@users.noreply.github.com>
2024-09-29 15:04:24 +0700

    first-commit

--- src/config.js
+++ src/config.js
@@ -1,6 +1,24 @@
+const fs = require("fs");
+const path = require("path");
+
+const ensureFileExists = (filePath) => {
+  if (!fs.existsSync(filePath)) {
+    fs.writeFileSync(filePath, "[]", "utf8");
+  }
+};
+
+// Remove one 'src' from the path
+const dbUserPath = path.join(__dirname, "data", "users.json");
+const dbCategoryPath = path.join(__dirname, "data", "categories.json");
+const dbRecipePath = path.join(__dirname, "data", "recipes.json");
+
+ensureFileExists(dbUserPath);
+ensureFileExists(dbCategoryPath);
+ensureFileExists(dbRecipePath);
+
 module.exports = {
   port: process.env.port || 3000,
-  dbUserPath: "./src/data/users.json",
-  dbCategoryPath: "./src/data/categories.json",
-  dbRecipePath: "./src/data/recipes.json",
+  dbUserPath,
+  dbCategoryPath,
+  dbRecipePath,
 };
